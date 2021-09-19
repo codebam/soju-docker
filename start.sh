b0136f1ab857967c8be556e42ccb92d356c3e5dd
Sean Behan <codebam@riseup.net>
2021-09-19 13:54:27 -0400

    update start script

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-podman run -it -v ./znc.sbehan.ca:/certs:z -v ./config:/config:z -v ./soju.db:/soju/soju.db:z soju /bin/sh
+podman run -d \
+	--name=soju \
+	-v /home/codebam/caddy/data/caddy/certificates/acme.zerossl.com-v2-dv90/znc.sbehan.ca:/certs:z \
+	-v ./config:/config:z \
+	-v ./soju.db:/soju/soju.db:z \
+	-p 6697:6697 \
+	soju
